2a8cbf460b1128e3d4e99837008a23d26933f2f1
Rafiklatrikdu69 <rafik.bouchenna050@email.com>
2024-03-28 05:44:08 +0100

    tests

---
+++ tests/UtilisateurTest.php
@@ -0,0 +1,32 @@
+<?php declare(strict_types=1);
+use PHPUnit\Framework\TestCase;
+
+class UtilisateurTest extends TestCase
+{
+    public function testCannotBeSame(): void
+    {
+        $this->expectException(Exception::class);
+
+        $user1 = new Utilisateur(
+            1, // ID
+            "pseudo123", // Pseudo
+            "exemple@example.com", // Email
+            "motdepasse123", // Mot de passe
+            100, // Point actuel
+            500, // Point classement
+            "actif", // Status
+            750 // Score de jeu
+        );
+        $user2 = new Utilisateur(
+            2, // ID
+            "pseudo123", // Pseudo (le même pseudo)
+            "exemple2@example.com", // Email
+            "motdepasse456", // Mot de passe
+            150, // Point actuel
+            600, // Point classement
+            "actif", // Status
+            800 // Score de jeu
+        );
+    }
+
+}
